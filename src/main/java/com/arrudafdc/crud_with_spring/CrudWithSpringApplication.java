80ef272f4646a3fbc88f6cf68b4859ad7fe4357c
arrudafdc <arrudafdc@gmail.com>
2024-06-09 02:09:22 -0300

    project created

---
+++ src/main/java/com/arrudafdc/crud_with_spring/CrudWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.arrudafdc.crud_with_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arrudafdc/crud_with_spring/CrudWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arrudafdc.crud_with_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
